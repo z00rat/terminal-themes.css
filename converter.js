aeeda5070ef66f004d555472c74cea02ca3a4b09
zoorat <78788887+z00rat@users.noreply.github.com>
2021-06-23 13:44:38 +0600

    adding chalk
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- converter.js
+++ converter.js
@@ -1,21 +1,23 @@
 const fs = require("fs");
 const { program } = require("commander");
-const yaml = require("js-yaml");
+const YAML = require("js-yaml");
+const chalk = require("chalk");
 
 program.version("0.0.1");
 program.requiredOption("-f, --file [letters...]", "location of the yaml file").option("-d, --debug", "output extra debugging");
 program.parse(process.argv);
 const options = program.opts();
 
-if (options.debug) {
-  console.log(options);
-}
+options.debug ? [console.log(chalk.yellow("arguments:")), console.log(options)] : [];
 
 for (let i = 0; i < options.file.length; i++) {
   try {
-    const doc = yaml.load(fs.readFileSync(options.file[i], "utf8"));
-    console.log(doc);
+    const doc = YAML.load(fs.readFileSync(options.file[i], "utf8"));
+    options.debug
+      ? [console.log(chalk.yellow("YAML to JSON of '") + chalk.underline.bold.cyan(options.file[i]) + chalk.yellow("':")), console.log(doc)]
+      : [];
   } catch (e) {
+    console.log(chalk.red("ERROR:"));
     console.log(e);
   }
 }
