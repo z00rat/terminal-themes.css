f267b579bb17b385ed946b41031dac4b09719e90
zoorat <78788887+z00rat@users.noreply.github.com>
2021-06-23 17:43:14 +0600

    parsing filename and big obj of all jsons
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- converter.js
+++ converter.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+var path = require("path");
 const { program } = require("commander");
 const YAML = require("js-yaml");
 const chalk = require("chalk");
@@ -14,14 +15,26 @@ program.parse(process.argv);
 const options = program.opts();
 options.debug ? [console.log(chalk.yellow("arguments:")), console.log(options)] : [];
 
+var JSONS = [];
 /**
  * looping over every file turn those from YAML to JSON
  */
 for (let i = 0; i < options.file.length; i++) {
   try {
-    const doc = YAML.load(fs.readFileSync(options.file[i], "utf8"));
+    JSONS[i] = YAML.load(fs.readFileSync(options.file[i], "utf8"));
+    /**
+     * @tutorial https://stackoverflow.com/a/50502331
+     */
+    JSONS[i].filename = path.basename(options.file[i], path.extname(options.file[i]));
     options.debug
-      ? [console.log(chalk.yellow("YAML to JSON of '") + chalk.underline.bold.cyan(options.file[i]) + chalk.yellow("':")), console.log(doc)]
+      ? [
+          console.log(
+            chalk.yellow(chalk.bold.redBright("[" + i + "] ") + "YAML to JSON of '") +
+              chalk.underline.bold.cyan(JSONS[i].filename) +
+              chalk.yellow(":")
+          ),
+          console.log(JSONS[i]),
+        ]
       : [];
   } catch (e) {
     console.log(chalk.red("ERROR:"));
