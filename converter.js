4d9f84e58cbea421a86d35d3c6393d81938bd983
zoorat <78788887+z00rat@users.noreply.github.com>
2021-06-23 22:53:39 +0600

    coded the compressed fullcssclass converter
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- converter.js
+++ converter.js
@@ -6,6 +6,7 @@ const chalk = require("chalk");
 const { toCSS, toJSON } = require("cssjson");
 var csso = require("csso");
 var css = require("css");
+const jsonToCss = require("json-to-css");
 
 /**
  * initiating argument parser to take input
@@ -46,6 +47,11 @@ for (let i = 0; i < options.file.length; i++) {
   }
 }
 
+/**
+ * to compress color value with csso
+ * @param {string} hex the hex value from YAML file
+ * @returns compressed color code
+ */
 function compressColor(hex) {
   var output;
   // console.log(hex);
@@ -56,60 +62,123 @@ function compressColor(hex) {
   // console.log(output);
   output = output.stylesheet.rules[0].declarations[0].value;
   // console.log(output);
+  /**
+   * creating json without '#' in color
+   * @tutorial https://stackoverflow.com/a/4564478
+   */
   output = output.substring(1);
 
   // console.log(output);
   return output;
 }
 
+function cssClassGen(clr, className, jsonCss) {
+  // console.log(clr);
+  // var jsonCss = {};
+  // var theCss = "";
+
+  className = "." + className;
+  jsonCss[className] = {};
+  jsonCss[className].color = "#" + clr;
+
+  className = className + "-bg";
+  jsonCss[className] = {};
+  jsonCss[className]["background-color"] = "#" + clr;
+  // theCss = jsonToCss.of(jsonCss);
+  // console.log(jsonCss);
+  // console.log(theCss);
+
+  return jsonCss;
+}
+
+/**
+ * json/css generate loop
+ */
 for (let i = 0; i < JSONS.length; i++) {
   try {
     input = JSONS[i].colors;
     filename = JSONS[i].filename;
-    output = {};
+    jsonOut = {};
+    jsonCss = {};
 
-    /**
-     * creating json without '#' in color
-     * @tutorial https://stackoverflow.com/a/4564478
-     */
-    output.bright = {};
-    output.bright.black = compressColor(input.bright.black);
-    output.bright.blue = compressColor(input.bright.blue);
-    output.bright.cyan = compressColor(input.bright.cyan);
-    output.bright.green = compressColor(input.bright.green);
-    output.bright.magenta = compressColor(input.bright.magenta);
-    output.bright.red = compressColor(input.bright.red);
-    output.bright.white = compressColor(input.bright.white);
-    output.bright.yellow = compressColor(input.bright.yellow);
-    output.normal = {};
-    output.normal.black = compressColor(input.normal.black);
-    output.normal.blue = compressColor(input.normal.blue);
-    output.normal.cyan = compressColor(input.normal.cyan);
-    output.normal.green = compressColor(input.normal.green);
-    output.normal.magenta = compressColor(input.normal.magenta);
-    output.normal.red = compressColor(input.normal.red);
-    output.normal.white = compressColor(input.normal.white);
-    output.normal.yellow = compressColor(input.normal.yellow);
-    output.primary = {};
-    output.primary.background = compressColor(input.primary.background);
-    output.primary.foreground = compressColor(input.primary.foreground);
-    output.selection = {};
-    output.selection.background = compressColor(input.selection.background);
-    output.selection.text = compressColor(input.selection.text);
-    output.cursor = {};
-    output.cursor.cursor = compressColor(input.cursor.cursor);
-    output.cursor.text = compressColor(input.cursor.text);
-    var fullJson = JSON.stringify(output);
+    isDebug ? [console.log(chalk.bgRedBright.black(filename + " :"))] : [];
+
+    jsonOut.normal = {};
+    jsonOut.bright = {};
+    jsonOut.primary = {};
+    jsonOut.selection = {};
+    jsonOut.cursor = {};
+    jsonOut.normal.black = compressColor(input.normal.black);
+    jsonOut.normal.blue = compressColor(input.normal.blue);
+    jsonOut.normal.cyan = compressColor(input.normal.cyan);
+    jsonOut.normal.green = compressColor(input.normal.green);
+    jsonOut.normal.magenta = compressColor(input.normal.magenta);
+    jsonOut.normal.red = compressColor(input.normal.red);
+    jsonOut.normal.white = compressColor(input.normal.white);
+    jsonOut.normal.yellow = compressColor(input.normal.yellow);
+    jsonOut.bright.black = compressColor(input.bright.black);
+    jsonOut.bright.blue = compressColor(input.bright.blue);
+    jsonOut.bright.cyan = compressColor(input.bright.cyan);
+    jsonOut.bright.green = compressColor(input.bright.green);
+    jsonOut.bright.magenta = compressColor(input.bright.magenta);
+    jsonOut.bright.red = compressColor(input.bright.red);
+    jsonOut.bright.white = compressColor(input.bright.white);
+    jsonOut.bright.yellow = compressColor(input.bright.yellow);
+    jsonOut.primary.background = compressColor(input.primary.background);
+    jsonOut.primary.foreground = compressColor(input.primary.foreground);
+    jsonOut.selection.background = compressColor(input.selection.background);
+    jsonOut.selection.text = compressColor(input.selection.text);
+    jsonOut.cursor.cursor = compressColor(input.cursor.cursor);
+    jsonOut.cursor.text = compressColor(input.cursor.text);
+
+    var FULLJSON = JSON.stringify(jsonOut);
 
     isDebug
       ? [
-          console.log(chalk.bgRedBright.black(filename + " :")),
           // console.log(chalk.bgBlueBright.black(JSON.stringify(input))),
-          console.log(chalk.bgBlueBright.black(JSON.stringify(output))),
+          console.log(chalk.bgBlueBright.black(JSON.stringify(jsonOut))),
         ]
       : [];
 
-    fs.writeFileSync("json/" + filename + ".json", fullJson);
+    jsonCss = cssClassGen(jsonOut.normal.black, "normal-black", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.blue, "normal-blue", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.cyan, "normal-cyan", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.green, "normal-green", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.magenta, "normal-magenta", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.red, "normal-red", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.white, "normal-white", jsonCss);
+    jsonCss = cssClassGen(jsonOut.normal.yellow, "normal-yellow", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.black, "bright-black", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.blue, "bright-blue", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.cyan, "bright-cyan", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.green, "bright-green", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.magenta, "bright-magenta", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.red, "bright-red", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.white, "bright-white", jsonCss);
+    jsonCss = cssClassGen(jsonOut.bright.yellow, "bright-yellow", jsonCss);
+    jsonCss = cssClassGen(jsonOut.primary.background, "primary-background", jsonCss);
+    jsonCss = cssClassGen(jsonOut.primary.foreground, "primary-foreground", jsonCss);
+    jsonCss = cssClassGen(jsonOut.selection.background, "selection-background", jsonCss);
+    jsonCss = cssClassGen(jsonOut.selection.text, "selection-text", jsonCss);
+    jsonCss = cssClassGen(jsonOut.cursor.cursor, "cursor-cursor", jsonCss);
+    jsonCss = cssClassGen(jsonOut.cursor.text, "cursor-text", jsonCss);
+
+    var CSSCLASS = csso.minify(jsonToCss.of(jsonCss)).css;
+
+    isDebug
+      ? [
+          console.log(jsonCss),
+          // console.log(cssClass),
+          console.log(chalk.bgMagentaBright.black(JSON.stringify(CSSCLASS))),
+        ]
+      : [];
+
+    //
+    console.log(chalk.redBright.bold("[ FULLJSON ]") + chalk.yellow(" writing to ") + chalk.magenta("json/" + filename + ".json"));
+    fs.writeFileSync("json/" + filename + ".json", FULLJSON);
+
+    console.log(chalk.redBright.bold("[ CSSCLASS ]") + chalk.yellow(" writing to ") + chalk.magenta("css-class/" + filename + ".css"));
+    fs.writeFileSync("css-class/" + filename + ".css", CSSCLASS);
   } catch (e) {
     console.log(chalk.red("ERROR:"));
     console.log(e);
