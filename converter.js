9cda95c7a4c98902688f176f1c6bb9ebd4922b51
zoorat <78788887+z00rat@users.noreply.github.com>
2021-06-23 13:48:18 +0600

    adding some comment
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- converter.js
+++ converter.js
@@ -3,13 +3,20 @@ const { program } = require("commander");
 const YAML = require("js-yaml");
 const chalk = require("chalk");
 
+/**
+ * initiating argument parser to take input
+ * use '-f <file paths>'
+ * or use '-d' for debug
+ */
 program.version("0.0.1");
 program.requiredOption("-f, --file [letters...]", "location of the yaml file").option("-d, --debug", "output extra debugging");
 program.parse(process.argv);
 const options = program.opts();
-
 options.debug ? [console.log(chalk.yellow("arguments:")), console.log(options)] : [];
 
+/**
+ * looping over every file turn those from YAML to JSON
+ */
 for (let i = 0; i < options.file.length; i++) {
   try {
     const doc = YAML.load(fs.readFileSync(options.file[i], "utf8"));
