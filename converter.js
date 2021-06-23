7595a2921038e55bf478259f7ccb60c4daaa38f3
zoorat <78788887+z00rat@users.noreply.github.com>
2021-06-23 20:40:08 +0600

    coded the compressed fulljson converter
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- converter.js
+++ converter.js
@@ -3,6 +3,9 @@ var path = require("path");
 const { program } = require("commander");
 const YAML = require("js-yaml");
 const chalk = require("chalk");
+const { toCSS, toJSON } = require("cssjson");
+var csso = require("csso");
+var css = require("css");
 
 /**
  * initiating argument parser to take input
@@ -13,7 +16,8 @@ program.version("0.0.1");
 program.requiredOption("-f, --file [letters...]", "location of the yaml file").option("-d, --debug", "output extra debugging");
 program.parse(process.argv);
 const options = program.opts();
-options.debug ? [console.log(chalk.yellow("arguments:")), console.log(options)] : [];
+isDebug = options.debug;
+isDebug ? [console.log(chalk.yellow("arguments:")), console.log(options)] : [];
 
 var JSONS = [];
 /**
@@ -26,7 +30,7 @@ for (let i = 0; i < options.file.length; i++) {
      * @tutorial https://stackoverflow.com/a/50502331
      */
     JSONS[i].filename = path.basename(options.file[i], path.extname(options.file[i]));
-    options.debug
+    isDebug
       ? [
           console.log(
             chalk.yellow(chalk.bold.redBright("[" + i + "] ") + "YAML to JSON of '") +
@@ -36,6 +40,76 @@ for (let i = 0; i < options.file.length; i++) {
           console.log(JSONS[i]),
         ]
       : [];
+  } catch (e) {
+    console.log(chalk.red("ERROR:"));
+    console.log(e);
+  }
+}
+
+function compressColor(hex) {
+  var output;
+  // console.log(hex);
+
+  output = csso.minify("p { color: " + hex + "; }").css;
+  // console.log(output);
+  output = css.parse(output);
+  // console.log(output);
+  output = output.stylesheet.rules[0].declarations[0].value;
+  // console.log(output);
+  output = output.substring(1);
+
+  // console.log(output);
+  return output;
+}
+
+for (let i = 0; i < JSONS.length; i++) {
+  try {
+    input = JSONS[i].colors;
+    filename = JSONS[i].filename;
+    output = {};
+
+    /**
+     * creating json without '#' in color
+     * @tutorial https://stackoverflow.com/a/4564478
+     */
+    output.bright = {};
+    output.bright.black = compressColor(input.bright.black);
+    output.bright.blue = compressColor(input.bright.blue);
+    output.bright.cyan = compressColor(input.bright.cyan);
+    output.bright.green = compressColor(input.bright.green);
+    output.bright.magenta = compressColor(input.bright.magenta);
+    output.bright.red = compressColor(input.bright.red);
+    output.bright.white = compressColor(input.bright.white);
+    output.bright.yellow = compressColor(input.bright.yellow);
+    output.normal = {};
+    output.normal.black = compressColor(input.normal.black);
+    output.normal.blue = compressColor(input.normal.blue);
+    output.normal.cyan = compressColor(input.normal.cyan);
+    output.normal.green = compressColor(input.normal.green);
+    output.normal.magenta = compressColor(input.normal.magenta);
+    output.normal.red = compressColor(input.normal.red);
+    output.normal.white = compressColor(input.normal.white);
+    output.normal.yellow = compressColor(input.normal.yellow);
+    output.primary = {};
+    output.primary.background = compressColor(input.primary.background);
+    output.primary.foreground = compressColor(input.primary.foreground);
+    output.selection = {};
+    output.selection.background = compressColor(input.selection.background);
+    output.selection.text = compressColor(input.selection.text);
+    output.cursor = {};
+    output.cursor.cursor = compressColor(input.cursor.cursor);
+    output.cursor.text = compressColor(input.cursor.text);
+    var fullJson = JSON.stringify(output);
+
+    isDebug
+      ? [
+          console.log(chalk.bgRedBright.black(filename + " :")),
+          // console.log(chalk.bgBlueBright.black(JSON.stringify(input))),
+          console.log(chalk.bgBlueBright.black(JSON.stringify(output))),
+        ]
+      : [];
+
+    fs.writeFileSync("json/" + filename + ".json", fullJson);
   } catch (e) {
     console.log(chalk.red("ERROR:"));
     console.log(e);
